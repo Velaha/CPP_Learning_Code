0283a1567b01aec344df26fb700da9277b2d272b
aheude <aheude>
2022-01-09 20:33:13 +0100

    done 3-array.cpp

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main()
-{
-    int array[] = { 0, 1, 2, 3 };
+int main(int argc, char** argv) {
+    using namespace std;
+    /*
+    int array[]    = { 0, 1, 2, 3 };
 
-    for (int i = 0; i < 4; ++i)
-    {
-        std::cout << array[i] << std::endl;
+    for (int val : array) {
+        cout << val << endl;
+    }
+    */
+    /*
+    int array[50] = {};
+
+    for (int i = 0; i < 50; i++) {
+        array[i] = i + 1;
+        cout << array[i] << endl;
+    }
+    */
+    vector<int> array;
+
+    if (argc < 2) {
+        cerr << "Missing argument" << endl;
+        return -1;
+    }
+    cout << "Argument : " << argv[1] << endl;
+
+    int length = stoi(argv[1]);
+    if (length <= 0) {
+        cerr << "Length must be > 0" << endl;
+        return -1;
+    }
+    cout << "Conversion : " << length << endl;
+
+    for (int i = 0; i < length; i++) {
+        array.emplace_back(i);
+    }
+
+    for (int value : array) {
+        cout << value << endl;
     }
 
     return 0;
