92571db7fdcc6d5ee717c727161c71dd873a622d
aheude <aheude>
2022-01-09 17:36:25 +0100

    done 1-hello_world.cpp

--- chap-01/1-hello_world.cpp
+++ chap-01/1-hello_world.cpp
@@ -1,1 +1,10 @@
 // Implement the program here.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "I had " << 5 << " cats." << std::endl;
+
+    return 0;
+}
