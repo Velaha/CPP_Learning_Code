ba0c3f6b7fbcd52c35d4ac19a9de388d5d983bf4
aheude <aheude>
2022-01-18 18:34:52 +0100

    done 4-rectangle

--- chap-02/4-rectangle/Rectangle.cpp
+++ chap-02/4-rectangle/Rectangle.cpp
@@ -1,1 +1,29 @@
-// Implement Rectangle functions here.
+#include "Rectangle.h"
+
+float Rectangle::_default_size = 3.f;
+
+Rectangle::Rectangle(float length, float width)
+    : _length { length }
+    , _width { width } {
+}
+
+Rectangle::Rectangle(float value)
+    : Rectangle(value, value) {
+}
+
+Rectangle::Rectangle()
+    : Rectangle(_default_size) {
+}
+
+void Rectangle::scale(float ratio) {
+    _length *= ratio;
+    _width *= ratio;
+}
+
+void Rectangle::set_default_size(float value) {
+    _default_size = value;
+}
+
+std::ostream& operator<<(std::ostream& out, const Rectangle& rectangle) {
+    return out << "{ L: " << rectangle.get_length() << ", W: " << rectangle.get_width() << " }";
+}
--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -1,23 +1,35 @@
 #pragma once
 
-class Rectangle
-{
+#include <ostream>
+
+class Rectangle {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    // Rectangle(float length, float width)
+    //     : _length { length }
+    //     , _width { width }
+    // {}
+    Rectangle(float length, float width);
+
+    Rectangle(float value);
+
+    Rectangle();
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    // void scale(float ratio) {
+    //     _length *= ratio;
+    //     _width *= ratio;
+    // }
+    void scale(float ratio);
+
+    static void set_default_size(float value);
 
 private:
+    static float _default_size;
+
     float _length;
     float _width;
 };
+
+std::ostream& operator<<(std::ostream& out, const Rectangle& rectangle);
