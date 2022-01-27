dd1669c610c091c6289fa23b9a5219362a546aab
aheude <aheude>
2022-01-27 18:37:40 +0100

    done 3-cat and 3-bag

--- chap-03/3-allocation/Cat.cpp
+++ chap-03/3-allocation/Cat.cpp
@@ -1,4 +1,27 @@
-int main()
-{
+#include "Cat.h"
+
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::unique_ptr<Cat> cat = std::make_unique<Cat>("Chat");
+    std::cout << *cat << std::endl;
+
+    std::unique_ptr<Cat> cat2;
+    if (cat2 == nullptr) {
+        std::cout << "nullptr ok " << std::endl;
+    }
+
+    cat2 = std::make_unique<Cat>("Chat2");
+
+    std::cout << "cat == cat2 : " << (cat == cat2) << std::endl;
+    std::cout << "*cat == *cat2 : " << (*cat == *cat2) << std::endl;
+
+    cat.reset();
+
+    if (cat == nullptr) {
+        std::cout << "nullptr ok " << std::endl;
+    }
+
     return 0;
 }
--- chap-03/3-allocation/Cat.h
+++ chap-03/3-allocation/Cat.h
@@ -3,15 +3,16 @@
 #include <iostream>
 #include <string>
 
-class Cat
-{
-    friend std::ostream& operator<<(std::ostream& stream, const Cat& cat)
-    {
+class Cat {
+    friend std::ostream& operator<<(std::ostream& stream, const Cat& cat) {
         return stream << "I have a cat named " << cat._name << ".";
     }
 
 public:
     bool operator==(const Cat& other) const { return _name == other._name; }
+    Cat(std::string name)
+        : _name { name } {}
+    ~Cat() { std::cout << "Cat destroyed" << std::endl; }
 
 private:
     std::string _name = "Felix";
