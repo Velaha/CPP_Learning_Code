8de07cfb308a8fd1e985cee48f2e7ba33f8d7422
aheude <aheude>
2022-01-28 17:35:44 +0100

    TEST : Trainers are constructed with their name and they know the PC where their Pokemons get sent

--- tp-03/Pokemons/Trainer.h
+++ tp-03/Pokemons/Trainer.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#include <string>
+
+class PC;
+
 // A person that capture Pokemons and make them fight.
-class Trainer
-{};
+class Trainer {
+public:
+    Trainer(std::string name, PC& pc)
+        : _name { name }
+        , _pc { pc } {}
+
+    std::string name() { return _name; }
+
+private:
+    std::string _name;
+    PC&         _pc;
+};
