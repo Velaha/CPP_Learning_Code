1dfc0f9f54d50755a926a0b54512fc73c2a7094d
aheude <aheude>
2022-01-28 16:55:25 +0100

    TEST : Pokemons can be constructed with their name

--- tp-03/Pokemons/Pokemon.h
+++ tp-03/Pokemons/Pokemon.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <string>
+
 // A creature that is cute and can fight other ones.
-class Pokemon
-{};
+class Pokemon {
+public:
+    Pokemon(std::string name)
+        : _name { name } {}
+
+    std::string name() { return _name; }
+
+private:
+    std::string _name;
+};
