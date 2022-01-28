4071a97698c55267aeec7054b7abc974b6d6d159
aheude <aheude>
2022-01-28 17:28:42 +0100

    TEST : A Pokemon can be put inside the PC

--- tp-03/Pokemons/PC.h
+++ tp-03/Pokemons/PC.h
@@ -2,15 +2,22 @@
 
 #include "Pokemon.h"
 
+#include <memory>
+#include <utility>
 #include <vector>
 
 // A PC is the place where Pokemons get automagically sent when the trainer's pockets are full.
 // When a Pokemon is transferred to the PC, this one takes ownership.
 class PC {
 public:
-    std::vector<Pokemon> pokemons() { return _pokemons; }
+    const std::vector<std::unique_ptr<Pokemon>>& pokemons() const { return _pokemons; }
+
+    void receive(std::unique_ptr<Pokemon> pokemon) {
+        _pokemons.emplace_back(std::move(pokemon));
+        _empty = false;
+    }
 
 private:
-    std::vector<Pokemon> _pokemons;
-    bool                 _empty = true;
+    std::vector<std::unique_ptr<Pokemon>> _pokemons;
+    bool                                  _empty = true;
 };
