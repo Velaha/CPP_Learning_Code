41339eaedf1fc33a27df9085b56d3dc11eb14d6c
aheude <aheude>
2022-01-29 18:59:09 +0100

    TEST : A trainer cannot steal the Pokemon of someone else

--- tp-03/Pokemons/PC.cpp
+++ tp-03/Pokemons/PC.cpp
@@ -3,10 +3,12 @@
 /*Ecrire le code de give_back et inclure Trainer.h dans le fichier PC.cpp évite une dépendance circulaire.*/
 #include "Trainer.h"
 
+#include <iostream>
+
 void PC::give_back(Trainer& trainer, const std::string& name) {
     for (auto index = _pokemons.begin(); index != _pokemons.end(); index++) {
         auto& poke = *index;
-        if (poke->name() == name) {
+        if (poke->trainer() == &trainer && poke->name() == name) {
             trainer.capture(std::move(poke));
             _pokemons.erase(index);
             return;
