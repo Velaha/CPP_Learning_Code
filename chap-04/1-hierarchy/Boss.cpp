29dfcf2515c71be88bcaa1d3ce3138fff1b06aa5
aheude <aheude>
2022-01-31 15:47:29 +0100

    done 1-hierarchy

--- chap-04/1-hierarchy/Boss.cpp
+++ chap-04/1-hierarchy/Boss.cpp
@@ -6,26 +6,16 @@
 #include <cstdio>
 
 Boss::Boss(std::string_view name, std::string_view surname, unsigned int salary, bool is_nice)
-    : _name { name }
-    , _surname { surname }
-    , _salary { salary }
-    , _is_nice { is_nice }
-{}
-
-void Boss::increase_salary(unsigned int raise)
-{
-    _salary += raise;
+    : Employee { name, surname, salary, Rank::TopManagement }
+    , _is_nice { is_nice } {
 }
 
-void Boss::assign_task(Staff& staff) const
-{
-    if (_is_nice)
-    {
+void Boss::assign_task(Staff& staff) const {
+    if (_is_nice) {
         std::printf("%s %s (Boss): \"Please %s, could you do this for me?\"\n", get_name().c_str(),
                     get_surname().c_str(), staff.get_name().c_str());
     }
-    else
-    {
+    else {
         std::printf("%s %s (Boss): \"%s! Hurry your ass up and finish me this right away!\"\n",
                     get_name().c_str(), get_surname().c_str(), staff.get_surname().c_str());
     }
@@ -33,15 +23,12 @@ void Boss::assign_task(Staff& staff) const
     staff.receive_task();
 }
 
-void Boss::ask_coffee(Intern& intern) const
-{
-    if (_is_nice)
-    {
+void Boss::ask_coffee(Intern& intern) const {
+    if (_is_nice) {
         std::printf("%s %s (Boss): \"Could you fetch me a coffee %s?\"\n", get_name().c_str(),
                     get_surname().c_str(), intern.get_name().c_str());
     }
-    else
-    {
+    else {
         std::printf("%s %s (Boss): \"Hey %s! Can't you see I'm thirsty ?!\"\n", get_name().c_str(),
                     get_surname().c_str(), intern.get_surname().c_str());
     }
--- chap-04/1-hierarchy/Intern.cpp
+++ chap-04/1-hierarchy/Intern.cpp
@@ -4,27 +4,18 @@
 #include <random>
 
 Intern::Intern(std::string_view name, std::string_view surname, unsigned int salary, bool is_clumsy)
-    : _name { name }
-    , _surname { surname }
-    , _salary { salary }
-    , _is_clumsy { is_clumsy }
-{}
-
-void Intern::increase_salary(unsigned int raise)
-{
-    _salary += raise;
+    : Employee { name, surname, salary, Rank::Slave }
+    , _is_clumsy { is_clumsy } {
 }
 
-void Intern::fetch_coffee()
-{
+void Intern::fetch_coffee() {
     std::random_device rd;
     std::mt19937       gen { rd() };
 
     const auto                  probability = _is_clumsy ? 0.4f : 0.1f;
     std::bernoulli_distribution dist { probability };
 
-    while (const auto break_cup = dist(gen))
-    {
+    while (const auto break_cup = dist(gen)) {
         std::printf("%s %s (Intern): \"Oups......\"\n", get_name().c_str(), get_surname().c_str());
         ++_nb_broken_cups;
     }
