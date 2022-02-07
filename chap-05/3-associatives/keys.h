56832e0555aef1ff949880b315fca1d53eaf6b29
aheude <aheude>
2022-02-07 21:21:10 +0100

    done 3-associatives

--- chap-05/3-associatives/keys.h
+++ chap-05/3-associatives/keys.h
@@ -1,29 +1,49 @@
 #pragma once
 
+#include <functional>
 #include <string>
 
-class ComparableDog
-{
+class ComparableDog {
 public:
     ComparableDog(const std::string& name, const std::string& species)
         : _name { name }
-        , _species { species }
-    {}
+        , _species { species } {}
+
+    bool operator<(const ComparableDog& other) const {
+        return _name < other._name || _species < other._species;
+    }
 
 private:
     std::string _name;
     std::string _species;
 };
 
-class HashableDog
-{
+class HashableDog {
 public:
     HashableDog(const std::string& name, const std::string& species)
         : _name { name }
-        , _species { species }
-    {}
+        , _species { species } {}
+
+    size_t get_hash() const {
+        std::hash<std::string> hash_fcn;
+        return hash_fcn(_name) ^ hash_fcn(_species);
+    }
+
+    bool operator==(const HashableDog& other) const {
+        return _name == other._name && _species == other._species;
+    }
 
 private:
     std::string _name;
     std::string _species;
 };
+
+namespace std {
+
+template <>
+struct hash<HashableDog>
+{
+    size_t operator()(const HashableDog& c) const { return c.get_hash(); }
+};
+
+} // namespace std
--- chap-05/3-associatives/main.cpp
+++ chap-05/3-associatives/main.cpp
@@ -1,4 +1,61 @@
-int main()
-{
+#include "keys.h"
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <unordered_set>
+
+int main() {
+    std::set<ComparableDog> comparableSet;
+
+    ComparableDog foxy { "Foxy", "Berger Allemand" };
+    comparableSet.emplace(foxy);
+    // Pour l'instant impossible car ComparableDog n'implémente pas l'opérateur <
+    ComparableDog jet { "Jetsan", "Lhassa Apso" };
+    comparableSet.emplace(jet);
+
+    std::cout << "Number of dogs : " << comparableSet.size() << std::endl;
+
+    ComparableDog foxy2 = foxy;
+    comparableSet.emplace(foxy2);
+    std::cout << "Number of dogs : " << comparableSet.size() << std::endl;
+
+    std::cout << "--------------" << std::endl;
+
+    std::unordered_set<HashableDog> hashableUnorderedSet;
+    // La classe HashableDog n'est pas encore hashable.
+
+    HashableDog mavrik { "Mavrik", "Berger des Pyrenees" };
+    hashableUnorderedSet.emplace(mavrik);
+    HashableDog duchesse { "Duchesse", "Patou" };
+    HashableDog navigue { "Navigue", "Colley" };
+
+    hashableUnorderedSet.emplace(navigue);
+    hashableUnorderedSet.emplace(duchesse);
+
+    std::cout << "Number of dogs : " << hashableUnorderedSet.size() << std::endl;
+
+    HashableDog mavrik2 = mavrik;
+    hashableUnorderedSet.emplace(mavrik2);
+    std::cout << "Number of dogs : " << hashableUnorderedSet.size() << std::endl;
+
+    std::cout << "--------------" << std::endl;
+
+    using Owner = std::pair<std::string, std::string>;
+
+    std::map<ComparableDog, Owner> comparableMap;
+
+    comparableMap.try_emplace(foxy, Owner { "Frederique", "Cottet" });
+    comparableMap.emplace(ComparableDog { "Neo", "Whippet" }, Owner { "Marianne", "Orzakiewicz" });
+    comparableMap.insert(std::make_pair(jet, Owner { "Andree", "Louis" }));
+    comparableMap.insert_or_assign(ComparableDog { "Rosa", "Husky" }, Owner { "Charlotte", "Becquin" });
+
+    comparableMap.emplace(foxy, Owner { "Vincent", "Heude" });            // ne fait rien
+    comparableMap.insert_or_assign(jet, Owner { "Christophe", "Heude" }); // changement de proprio
+
+    // Si on veut maintenant utiliser une unordered_map, il faut changer le type des clées par des
+    // HashableDog.
+
     return 0;
 }
