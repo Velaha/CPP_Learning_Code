07d4f377399e8242331e06af5457bc8b800c66c7
aheude <aheude>
2022-02-07 23:31:33 +0100

    done 4-strings

--- chap-05/4-strings.cpp
+++ chap-05/4-strings.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <string_view>
 
-void print_in_console(const std::string& str)
-{
+void print_in_console(std::string_view str) {
     std::cout << str << std::endl;
 }
 
-int main()
-{
+int main() {
     print_in_console("print me");
+
+    // concaténation -> push_back, append, +=, +
+    // extraction de sous-chaine -> substr
+    // recherche de caractère ou de sous-chaine -> contains (, starts_with, ends_with), find family
+    // comparaison -> compare, ==
+    // remplacement d'une partie de la chaine -> replace
+
+    // conversion de valeurs numériques en chaine de caratères -> to_string
+
+    // conversion d'une chaine de caractères en valeur numérique -> sto.i / l(l) / ul(l) / f / (l)d
+    // si la chaine ne contient pas vraiment de nombre alors le compilateur renvoie une
+    // std::invalid_argument.
+
     return 0;
 }
